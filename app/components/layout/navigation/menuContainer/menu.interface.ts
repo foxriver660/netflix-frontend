8bc244803badac64b4fe26c8185225dffce1d6ae
foxriver <foxriver660@gmail.com>
2023-12-01 17:08:39 +0300

    feat: add menu data

---
+++ app/components/layout/navigation/menuContainer/menu.interface.ts
@@ -0,0 +1,6 @@
+import { IMenuItem } from './menuItem/menuItem.interface'
+
+export interface IMenu {
+	title: string
+	items: IMenuItem[]
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,121 +1,121 @@
-import type { Config } from "tailwindcss";
-import colors from "tailwindcss/colors";
-import plugin from "tailwindcss/plugin";
+import type { Config } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
+import plugin from 'tailwindcss/plugin'
 
-const primary = "#E30B13";
+const primary = '#E30B13'
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    colors: {
-      primary: primary,
-      black: colors.black,
-      white: colors.white,
-      transparent: colors.transparent,
-      yellow: {
-        700: "#F5C521",
-      },
-      gray: {
-        300: "#E0E0E0",
-        500: "#9E9E9E",
-        600: "#757575",
-        700: "#616161",
-        800: "#424242",
-        900: "#212121",
-        950: "#1A1A1A",
-      },
-    },
-    extend: {
-      spacing: { 0.5: "0.12rem", layout: "2.75rem" },
-      fontSize: {
-        "2lg": "1.38rem",
-      },
-      borderRadius: {
-        image: "0.5rem",
-        layout: "0.5rem",
-      },
-      transitionTimingFunction: {
-        DEFAULT: "easy-in-out",
-      },
-      transitionDuration: {
-        DEFAULT: "200ms",
-      },
-      zIndex: {
-        1: "1",
-        2: "2",
-        3: "3",
-      },
-      keyframes: {
-        fade: {
-          from: { opacity: "0" },
-          to: { opacity: "1" },
-        },
-        scaleIn: {
-          "0%": { opacity: "0", transform: "scale(0.9)" },
-          "50%": { opacity: "0.3" },
-          "100%": { opacity: "1", transform: "scale(1)" },
-        },
-      },
-      animation: {
-        fade: "fade 200ms ease-in-out",
-        scaleIn: "scaleIn 200ms ease-in-out",
-      },
-    },
-  },
-  plugins: [
-    plugin(({ addComponents, theme, addUtilities }) => {
-      addComponents({
-        ".btn-primary": {
-          backgroundColor: primary,
-          color: "#fff",
-          borderRadius: "0.65rem",
-          transition: "background-color 200ms ease-in-out",
-          "&:hover": {
-            backgroundColor: "#ff0009",
-          },
-        },
-        ".text-link": {
-          textUnderlineOffset: "4",
-          color: "rgba(255, 255, 255, .9)",
-          transition: "text-decoration-color 200ms ease-in-out",
-          textDecorationLine: "underline",
-          textDecorationColor: "rgba(255, 255, 255,.2)",
-          "&:hover": {
-            textDecorationColor: "rgba(255, 255, 255,.9)",
-          },
-        },
-        ".air-block": {
-          borderRadius: theme("borderRadius.layout"),
-          backgroundColor: theme("colors.gray.950"),
-          color: theme("colors.white"),
-          margin: theme("spacing.2"),
-          boxShadow: theme("boxShadow.lg"),
-        },
-      });
-      addUtilities({
-        ".text-shadow": {
-          textShadow: "1px 1px rgba(0, 0, 0, 0.4)",
-        },
-        ".outline-border-none": {
-          outline: "none",
-          border: "none",
-        },
-        ".flex-center-between": {
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        },
-        ".image-like-bg": {
-          objectPosition: "center",
-          objectFit: "cover",
-          pointerEvents: "none",
-        },
-      });
-    }),
-  ],
-};
-export default config;
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		colors: {
+			primary: primary,
+			black: colors.black,
+			white: colors.white,
+			transparent: colors.transparent,
+			yellow: {
+				700: '#F5C521',
+			},
+			gray: {
+				300: '#E0E0E0',
+				500: '#9E9E9E',
+				600: '#757575',
+				700: '#616161',
+				800: '#424242',
+				900: '#212121',
+				950: '#1A1A1A',
+			},
+		},
+		extend: {
+			spacing: { 0.5: '0.12rem', layout: '2.75rem' },
+			fontSize: {
+				'2lg': '1.38rem',
+			},
+			borderRadius: {
+				image: '0.5rem',
+				layout: '0.5rem',
+			},
+			transitionTimingFunction: {
+				DEFAULT: 'easy-in-out',
+			},
+			transitionDuration: {
+				DEFAULT: '200ms',
+			},
+			zIndex: {
+				1: '1',
+				2: '2',
+				3: '3',
+			},
+			keyframes: {
+				fade: {
+					from: { opacity: '0' },
+					to: { opacity: '1' },
+				},
+				scaleIn: {
+					'0%': { opacity: '0', transform: 'scale(0.9)' },
+					'50%': { opacity: '0.3' },
+					'100%': { opacity: '1', transform: 'scale(1)' },
+				},
+			},
+			animation: {
+				fade: 'fade 200ms ease-in-out',
+				scaleIn: 'scaleIn 200ms ease-in-out',
+			},
+		},
+	},
+	plugins: [
+		plugin(({ addComponents, theme, addUtilities }) => {
+			addComponents({
+				'.btn-primary': {
+					backgroundColor: primary,
+					color: '#fff',
+					borderRadius: '0.65rem',
+					transition: 'background-color 200ms ease-in-out',
+					'&:hover': {
+						backgroundColor: '#ff0009',
+					},
+				},
+				'.text-link': {
+					textUnderlineOffset: '4',
+					color: 'rgba(255, 255, 255, .9)',
+					transition: 'text-decoration-color 200ms ease-in-out',
+					textDecorationLine: 'underline',
+					textDecorationColor: 'rgba(255, 255, 255,.2)',
+					'&:hover': {
+						textDecorationColor: 'rgba(255, 255, 255,.9)',
+					},
+				},
+				'.air-block': {
+					borderRadius: theme('borderRadius.layout'),
+					backgroundColor: theme('colors.gray.950'),
+					color: theme('colors.white'),
+					margin: theme('spacing.2'),
+					boxShadow: theme('boxShadow.lg'),
+				},
+			})
+			addUtilities({
+				'.text-shadow': {
+					textShadow: '1px 1px rgba(0, 0, 0, 0.4)',
+				},
+				'.outline-border-none': {
+					outline: 'none',
+					border: 'none',
+				},
+				'.flex-center-between': {
+					display: 'flex',
+					justifyContent: 'space-between',
+					alignItems: 'center',
+				},
+				'.image-like-bg': {
+					objectPosition: 'center',
+					objectFit: 'cover',
+					pointerEvents: 'none',
+				},
+			})
+		}),
+	],
+}
+export default config
